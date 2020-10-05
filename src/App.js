fe2e2f9767049283d0979ccc2c5b584d4a9f7a5b
gbronca <42703789+gbronca@users.noreply.github.com>
2020-10-05 20:05:08 +0100

    Added Clarifai face recognition API

--- src/App.js
+++ src/App.js
@@ -3,8 +3,15 @@ import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import Signin from './components/Signin/Signin';
 import Particles from 'react-particles-js';
 import './App.css';
+
+import Clarifai from 'clarifai';
+const app = new Clarifai.App({
+  apiKey: '35832779b8d64d96a7d153286574e9f2'
+});
 
 const particlesOptions = {
   particles: {
@@ -18,17 +25,69 @@ const particlesOptions = {
   }
 }
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+      box: {},
+      route: 'signin'
+    }
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height),
+    }
+  }
+
+  displayFaceBox = (box) => this.setState({box});
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input})
+    app.models
+      .predict(
+        'c0c0ac362b03416da06ab3fa36fb58e3',
+        this.state.input
+      )
+      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .catch(err => console.log(err));
+}
+
+onRouteChange = (route) => {
+  this.setState({route: route});
+}
+
   render() {
     return (
       <div className="App">
         <Particles className='particles'
           params={particlesOptions}
         />
-        <Navigation />
-        <Logo />
-        <Rank />
-        <ImageLinkForm />
-        {/* <FaceRecognition /> */}
+        <Navigation onRouteChange={this.onRouteChange} />
+        { this.state.route === 'signin'
+          ? <Signin onRouteChange={this.onRouteChange} />
+          : <div>
+              <Logo />
+              <Rank />
+              <ImageLinkForm
+                onInputChange={this.onInputChange}
+                onButtonSubmit={this.onButtonSubmit}
+              />
+              <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
+            </div>
+        }
       </div>
     );
   }
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import './ImageLinkForm.css';
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
     return(
       <div>
         <p className='f3'>
             {'This Magic Brain will detect faces in your pictures'}
         </p>
         <div className='center'>
-            <div className='form center pa4 br3 shadow-2'>
-                <input type="text" className='f4 pa2 w-70 center' />
-                <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>Detect</button>
+            <div className='form center pa4 br3 shadow-2 w-50'>
+                <input type="text" className='f4 pa2 w-70 center' onChange={onInputChange} />
+                <button
+                  className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'
+                  onClick={onButtonSubmit}>Detect</button>
             </div>
         </div>
       </div>
