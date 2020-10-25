9a780256096c70cf2245f08e98b9cfd1d1f80b58
gbronca <42703789+gbronca@users.noreply.github.com>
2020-10-25 17:52:48 +0000

    replaced localhost with remote api address

--- src/App.js
+++ src/App.js
@@ -72,7 +72,7 @@ class App extends React.Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-    fetch('http://localhost:3000/image', {
+    fetch('https://gb-smart-brain-api.herokuapp.com/image', {
       method: 'post',
       headers: { 'content-type': 'application/json'},
       body: JSON.stringify({
@@ -82,7 +82,7 @@ class App extends React.Component {
     .then(response => response.json())
     .then(response => {
       if (response) {
-        fetch('http://localhost:3000/image', {
+        fetch('https://gb-smart-brain-api.herokuapp.com/image', {
           method: 'put',
           headers: { 'content-type': 'application/json'},
           body: JSON.stringify({
